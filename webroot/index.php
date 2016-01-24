8325292e87e3fd990ebb4eabd04f85897a5f1086
Paul Hoang <paulkhoahoang@gmail.com>
2016-01-24 04:18:48 +0000

    Add lucky money description

--- webroot/index.php
+++ webroot/index.php
@@ -220,8 +220,9 @@
                 <img src="https://placehold.it/1000x1000" class="img-responsive"/>
             </div>
             <div class="col-sm-8 wow fadeInRight" data-wow-offset="100">
-                <h2>Wishing Well</h2>
-                <p></p>
+                <h2>Wish us luck (Red Envelope)</h2>
+                <p>If you want to wish us luck in the future, money in a red envelope is the perfect gift. "li xi" (Vietnamese: pronounced <i>"lee see"</i>) is typically given to the bride and groom to signify luck and happiness.</p>
+                <p>We will be providing red envelopes at the wedding and the reception. Any amount is appreciated, as it's the thought that counts.</p>
             </div>
         </div>
         <hr class="or-horizontal-separator" />
