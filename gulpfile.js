415bbe3c302d6ee78d6e23320852dfb5822e7a64
Paul Hoang <paulkhoahoang@gmail.com>
2016-02-27 12:20:39 +0000

    Add template concatenation to gulp compile task
    The HTML template concatenation was once called from a bash script.
    It's now moved into the "compile" gulp task and the bash script is
    now removed.

--- gulpfile.js
+++ gulpfile.js
@@ -2,15 +2,20 @@ var gulp = require('gulp');
 var gutil = require('gulp-util');
 var rename = require('gulp-rename');
 var sass = require('gulp-sass');
+var concat =require('gulp-concat');
 
 gulp.task('default', function() {
 	// Empty
 });
 
 gulp.task('compile', function() {
-	return gulp.src('src/Sass/styles.scss')
+	gulp.src('src/Sass/styles.scss')
 		.pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
 		.pipe(rename('styles.css'))
 		.pipe(gulp.dest('webroot/assets/css/'));
+
+	gulp.src(['templates/*.html', '!templates/all.html'])
+		.pipe(concat('all.html'))
+		.pipe(gulp.dest('templates/'));
 });
 
